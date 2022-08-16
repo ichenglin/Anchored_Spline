968788bd6473aa01543eddc77257e15dffa8290c
Icheng Lin <fireclaws9@gmail.com>
2022-08-16 12:35:04 +0800

    Removed Debug Comment

--- source/objects/matrix.ts
+++ source/objects/matrix.ts
@@ -18,10 +18,6 @@ export class Matrix {
     }
 
     private cramer_rule_diagonal(matrix: number[][], direction: "positive" | "negative"): number {
-        //console.log(matrix);
-        /*if (matrix.length !== matrix[0].length) {
-            throw Error(`Invalid matrix for Cramer's Rule. (${matrix.length}x${matrix[0].length})`);
-        }*/
         const matrix_size = matrix.length;
         let matrix_sum = 0;
         for (let column_index = 0; column_index < matrix_size; column_index++) {
@@ -29,9 +25,7 @@ export class Matrix {
             for (let row_index = 0; row_index < matrix_size; row_index++) {
                 const column_offset = (direction === "positive" ? row_index : matrix_size - row_index - 1);
                 const row_offset = (row_index + column_index) % 3;
-                //console.log(matrix[column_offset]);
                 diagonal_product *= matrix[column_offset][row_offset];
-                //console.log(`[${column_offset}][${row_offset}]`);
             }
             matrix_sum += diagonal_product;
         }
